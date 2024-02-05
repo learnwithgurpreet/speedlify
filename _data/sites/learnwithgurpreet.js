4ac95ce143a4b4cc8e4e4c53b14f13ab1fffca60
Gurpreet Singh <gursingh7@publicisgroupe.net>
2024-02-05 09:41:05 +0100

    changed site

---
+++ _data/sites/learnwithgurpreet.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "Learnwithgurpreet", // optional, falls back to object key
+  description: "Every space has a story.",
+  options: {
+    runs: 1,
+    frequency: 60 * 23, // 24 hours
+  },
+  urls: [
+    "https://www.learnwithgurpreet.com/",
+    "https://www.learnwithgurpreet.com/posts/",
+    "https://www.learnwithgurpreet.com/stats/",
+    "https://www.learnwithgurpreet.com/tags/",
+    "https://www.learnwithgurpreet.com/contact/",
+  ],
+};
